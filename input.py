1715e01949910bf3927a6bc810ff066ca062e8b3
Hasan-ul-Islam Shaffy <hasanshofi49@gmail.com>
2025-07-09 10:59:16 -0400

    initial commit

---
+++ input.py
@@ -0,0 +1,23 @@
+#a function that promps a user to enter data returns the entered data asa a string
+# name = input('What is your name?')
+# age = input('How old are you')
+# age = int(age)
+# new_age = age + 1
+#
+# print(f'Hello {name}')
+# print(f"You are {new_age} years old")
+
+#Exercie-1
+
+# length = float(input("What is the length"))
+# width = float(input("What is the width"))
+# area = length*width
+# print(f'The are is {area} cm')
+
+#Exercise-2
+
+item = input("What do you like to buy")
+price = float(input("what is the price?"))
+quantity = int(input("How many you want?"))
+total = price*quantity
+print(f"Your {item} is {total}$ for {quantity} pieces")
